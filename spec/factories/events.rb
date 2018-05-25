7bee6f401837c0a76c6b1ab76820b6ccb002a85b
Shinsuke Kido <downmap555@gmail.com>
2018-05-25 16:07:41 +0900

    $ bundle exec rails g resource event owner_id:integer name:string place:string start_time:datetime end_time:datetime content:text コマンドの実行

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :event do
+    owner_id 1
+    name 'MyString'
+    place 'MyString'
+    start_time '2018-05-25 16:06:22'
+    end_time '2018-05-25 16:06:22'
+    content 'MyText'
+  end
+end
