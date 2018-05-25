6e320739daf2098a03fc3864e0a96a178229252c
Shinsuke Kido <downmap555@gmail.com>
2018-05-25 16:52:09 +0900

    FactoryBot の値を修正。

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :event do
     owner_id 1
-    name 'MyString'
-    place 'MyString'
+    name 'meetup'
+    place 'shinjuku'
     start_time '2018-05-25 16:06:22'
-    end_time '2018-05-25 16:06:22'
+    end_time '2018-05-25 17:06:22'
     content 'MyText'
   end
 end
