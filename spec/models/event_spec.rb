e12bcc3c5b5d1505a48a5e1e9fffa19245e10b30
Shinsuke Kido <downmap555@gmail.com>
2018-05-25 16:52:53 +0900

    Event model のRSpec Test の作成。

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context '正常に値が保存される場合' do
+      it 'is valid with name, place, content, start_time, end_time' do
+        event = build(:event)
+        expect(event).to be_valid
+      end
+    end
+
+    context '未入力のカラムが存在する場合' do
+      it 'is invalid without name' do
+        event = build(:event, name: nil)
+        event.valid?
+        expect(event.errors[:name]).to include("can't be blank")
+      end
+
+      it 'is invalid without place' do
+        event = build(:event, place: nil)
+        event.valid?
+        expect(event.errors[:place]).to include("can't be blank")
+      end
+
+      it 'is invalid without content' do
+        event = build(:event, content: nil)
+        event.valid?
+        expect(event.errors[:content]).to include("can't be blank")
+      end
+
+      it 'is invalid without start_time' do
+        event = build(:event, start_time: nil)
+        event.valid?
+        expect(event.errors[:start_time]).to include("can't be blank")
+      end
+
+      it 'is invalid without end_time' do
+        event = build(:event, end_time: nil)
+        event.valid?
+        expect(event.errors[:end_time]).to include("can't be blank")
+      end
+    end
+
+    context '文字数が制限値より多いカラムが存在する場合' do
+      it 'is invalid with name that has more than 50 characters ' do
+        event = build(:event, name: 'a' * 51)
+        event.valid?
+        expect(event.errors[:name][0]).to include('is too long')
+      end
+
+      it 'is invalid with place that has more than 100 characters ' do
+        event = build(:event, place: 'a' * 101)
+        event.valid?
+        expect(event.errors[:place][0]).to include('is too long')
+      end
+
+      it 'is invalid with content that has more than 2000 characters ' do
+        event = build(:event, content: 'a' * 2001)
+        event.valid?
+        expect(event.errors[:content][0]).to include('is too long')
+      end
+    end
+
+    it 'is invalid with start_time that has larger number than end_time ' do
+      event = build(:event, start_time: '2018-05-24 14:10:05', end_time: '2018-05-23 14:10:05')
+      event.valid?
+      expect(event.errors[:start_time][0]).to include('は終了時間よりも前に設定してください')
+    end
+  end
 end
