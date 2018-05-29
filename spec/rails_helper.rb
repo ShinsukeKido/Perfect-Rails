1ea3d853664b954031cfecc65159cbcd720b604a
Shinsuke Kido <downmap555@gmail.com>
2018-05-29 12:13:23 +0900

    shoulda-matchers をテストで使用できるよう、rails_helper.rb に追記。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,3 +57,10 @@ RSpec.configure do |config|
 
   OmniAuth.config.test_mode = true
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
