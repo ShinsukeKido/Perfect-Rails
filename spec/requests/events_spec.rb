f9707501689ce60b545e8dafb60cf57d1c4c7b9a
Shinsuke Kido <downmap555@gmail.com>
2018-05-31 19:33:54 +0900

    EventsController#show のRequestSpec を作成（同時に、ログインが必要なアクションと必要でないアクションをcontext を用いて場合分け）

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,68 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe 'EventsController', type: :request do
-  before do
-    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
-      provider: 'twitter',
-      uid: '0123456789',
-      info: {
-        nickname: 'hogehoge',
-        image: 'http://image.example.com',
-      },
-    )
-    get '/auth/twitter/callback'
-  end
-
-  describe '#new' do
-    it '@event が新しく作成される' do
-      get '/events/new'
-      expect(assigns(:event)).to be_a_new(Event)
+  context 'ログインが必要な場合' do
+    before do
+      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
+        provider: 'twitter',
+        uid: '0123456789',
+        info: {
+          nickname: 'hogehoge',
+          image: 'http://image.example.com',
+        },
+      )
+      get '/auth/twitter/callback'
     end
 
-    it 'new.html.erb ページに遷移する' do
-      get '/events/new'
-      expect(response).to render_template :new
-    end
-  end
-
-  describe '#create' do
-    context 'イベント作成ページで、正しい値が入力された場合' do
-      let(:params) do
-        {
-          event: {
-            name: 'event',
-            place: 'place',
-            content: 'sentence',
-            start_time: Time.zone.local(2018, 5, 28, 14, 0o0),
-            end_time: Time.zone.local(2018, 5, 28, 15, 0o0),
-          },
-        }
-      end
-
-      it 'イベントを新規作成する' do
-        expect { post '/events', params: params }.to change { Event.count }.by(1)
-      end
-    end
-    context 'イベント作成ページで、正しくない値が入力された場合' do
-      let(:params) do
-        {
-          event: {
-            name: 'event',
-            place: 'tokyo',
-            content: 'sentence',
-            start_time: Time.zone.local(2018, 5, 28, 14, 0o0),
-            end_time: Time.zone.local(2018, 5, 28, 14, 0o0),
-          },
-        }
-      end
-
-      it '新しいイベントが作成されない' do
-        expect { post '/events', params: params }.not_to change { Event.count }
+    describe '#new' do
+      it '@event が新しく作成される' do
+        get '/events/new'
+        expect(assigns(:event)).to be_a_new(Event)
       end
 
       it 'new.html.erb ページに遷移する' do
-        post '/events', params: params
+        get '/events/new'
         expect(response).to render_template :new
+      end
+    end
+
+    describe '#create' do
+      context 'イベント作成ページで、正しい値が入力された場合' do
+        let(:params) do
+          {
+            event: {
+              name: 'event',
+              place: 'place',
+              content: 'sentence',
+              start_time: Time.zone.local(2018, 5, 28, 14, 0o0),
+              end_time: Time.zone.local(2018, 5, 28, 15, 0o0),
+            },
+          }
+        end
+
+        it 'イベントを新規作成する' do
+          expect { post '/events', params: params }.to change { Event.count }.by(1)
+        end
+      end
+      context 'イベント作成ページで、正しくない値が入力された場合' do
+        let(:params) do
+          {
+            event: {
+              name: 'event',
+              place: 'tokyo',
+              content: 'sentence',
+              start_time: Time.zone.local(2018, 5, 28, 14, 0o0),
+              end_time: Time.zone.local(2018, 5, 28, 14, 0o0),
+            },
+          }
+        end
+
+        it '新しいイベントが作成されない' do
+          expect { post '/events', params: params }.not_to change { Event.count }
+        end
+
+        it 'new.html.erb ページに遷移する' do
+          post '/events', params: params
+          expect(response).to render_template :new
+        end
+      end
+    end
+  end
+
+  context 'ログインが不要な場合' do
+    describe '#show' do
+      let(:event) { create(:event, owner_id: create(:user).id) }
+
+      it '@event にevents テーブルのレコードが格納されている' do
+        get "/events/#{event.id}"
+        expect(assigns(:event)).to eq event
+      end
+
+      it 'show.html.erb ページに遷移する' do
+        get "/events/#{event.id}"
+        expect(response).to render_template :show
       end
     end
   end
