e38f28628d021ebe7ea020435e2aa123138d3d6c
Shinsuke Kido <downmap555@gmail.com>
2018-06-20 19:26:26 +0900

    UsersController の作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate
+
+  def destroy
+    if current_user.destroy
+      session.delete(:user_id)
+      redirect_to root_path, notice: '退会完了しました'
+    else
+      render :retire
+    end
+  end
 end
